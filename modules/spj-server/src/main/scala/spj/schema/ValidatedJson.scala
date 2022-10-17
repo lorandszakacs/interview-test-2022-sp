0bda6888ecfdeddc04e8d360ade52ee09ec4906e
Loránd Szakács <lorand.szakacs@protonmail.com>
2022-10-17 15:02:17 +0300

    Implement JSON validation

--- modules/spj-server/src/main/scala/spj/schema/ValidatedJson.scala
+++ modules/spj-server/src/main/scala/spj/schema/ValidatedJson.scala
@@ -2,10 +2,32 @@ package spj.schema
 
 import io.circe.Encoder
 import io.circe.Json
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.fge.jsonschema.main.{JsonSchemaFactory, JsonValidator}
+import com.github.fge.jsonschema.processors.syntax.SyntaxValidator
 import spj.*
+
+import scala.jdk.CollectionConverters.*
 
 type ValidatedJson = ValidatedJson.Type
 object ValidatedJson extends monix.newtypes.Newsubtype[Json] {
   given Encoder[ValidatedJson] = derive[Encoder]
-  def validate[F[_]: ApplicativeThrow](rawJson: Json, schema: JsonSchema): F[ValidatedJson] = ????[F, ValidatedJson]
+
+  private val jsdkObjectMapper: ObjectMapper = new ObjectMapper()
+  private val validator: JsonValidator = JsonSchemaFactory.byDefault().getValidator
+
+  def validate[F[_]: ApplicativeThrow](rawJson: Json, schema: JsonSchema): F[ValidatedJson] = {
+    val schemaNode: JsonNode = jsdkObjectMapper.readTree(schema.value.noSpacesSortKeys)
+    val cleanedInput = rawJson.deepDropNullValues
+    val inputNode: JsonNode = jsdkObjectMapper.readTree(cleanedInput.noSpacesSortKeys)
+    val report = validator.validate(schemaNode, inputNode)
+    if (report.isSuccess) {
+      this.unsafeCoerce(cleanedInput).pure[F]
+    } else {
+      Anomaly
+        .invalidInput(report.iterator().asScala.map(msg => msg.getMessage).mkString(";"))
+        .raiseError[F, ValidatedJson]
+    }
+  }
 }
