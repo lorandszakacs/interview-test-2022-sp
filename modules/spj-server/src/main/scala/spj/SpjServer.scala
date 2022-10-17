f2bc3832a967db915dfeb0b6dd43988973a34729
Loránd Szakács <lorand.szakacs@protonmail.com>
2022-10-17 12:35:53 +0300

    Add flyway migrate on server init

--- modules/spj-server/src/main/scala/spj/SpjServer.scala
+++ modules/spj-server/src/main/scala/spj/SpjServer.scala
@@ -2,21 +2,23 @@ package spj
 
 import fs2.io.net.Network
 import spj.db.*
+import spj.db.flyway.Flyway
 import spj.schema.api.SpjApi
 import spj.schema.routes.SpjHttp4sRoutes
 
-case class SpjServer[F[_]] private (
+final case class SpjServer[F[_]] private (
     api: SpjApi[F],
     routes: SpjHttp4sRoutes[F]
 )
 
 object SpjServer {
   def make[F[_]](config: SpjConfig)(using
-      F: Concurrent[F],
+      F: Async[F],
       network: Network[F],
       console: Console[F]
   ): Resource[F, SpjServer[F]] = {
     for {
+      _ <- Flyway.migrate(config.dbConfig)
       db <- SessionPool.make(config.dbConfig)
       api <- SpjApi.server[F](db)
       routes <- SpjHttp4sRoutes.make[F](api)
